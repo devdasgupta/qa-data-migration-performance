4299055a248d865cc42aed6d0a0ae80e83e8ac01
Devdutta Dasgupta <dev.dasgupta@syapse.com>
2019-02-12 11:11:15 -0500

    Updated Test script

--- script/execute_test.py
+++ script/execute_test.py
@@ -1,7 +1,7 @@
 import uuid
 import boto3
 import socket
-import os
+from datetime import datetime
 
 
 class S3Activity:
@@ -19,15 +19,17 @@ class S3Activity:
         s3_object = self.s3_resource.Object(bucket.name, full_path)
         s3_object.upload_file(filename)
 
-    def get_s3_details(self, bucket_name):
-        bucket = self.s3_resource.Bucket(name=bucket_name)
+    def get_s3_details(self, folder):
+        bucket = self.s3_resource.Bucket(name=self.bucket_name)
 
         msg_list = list()
 
         for obj in bucket.objects.all():
             fname = obj.key
-            s3_obj = self.s3_resource.Object(bucket_name, fname)
-            msg_list.append('{} {} 1'.format(fname, s3_obj.last_modified))
+            if fname.startswith(folder):
+                s3_obj = self.s3_resource.Object(self.bucket_name, fname)
+                tstamp = int(datetime.strptime(str(s3_obj.last_modified), '%Y-%m-%d %H:%M:%S+00:00').strftime("%s"))
+                msg_list.append('{} 1 {}'.format(self.bucket_name, tstamp))
 
         return msg_list
 
@@ -37,29 +39,25 @@ class GraphiteClient:
     def __init__(self, host):
         self.host = host
         self.port = 2003
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.host, self.port))
 
-    def graphite_connect(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((self.host, self.port))
-        return sock
+    def send_message(self, name, value, tstamp):
+        graphite_msg = "%s %s %s\n" % (name, value, tstamp)
+        self.sock.sendall(graphite_msg.encode('utf-8'))
 
-    def send_message(self, sock, path, msg):
-        graphite_msg = "%s %s %d" % (path, msg, 1)
-        sock.sendall(graphite_msg.encode('utf-8'))
-
-    # def __del__(self, sock):
-    #     try:
-    #         sock.shutdown(socket.SHUT_RDWR)
-    #         sock.close()
-    #     except Exception as e:
-    #         sock.close()
-    #         print('Socket Exception: ' + str(e))
+    def __del__(self):
+        try:
+            self.sock.shutdown(socket.SHUT_RDWR)
+            self.sock.close()
+        except Exception as e:
+            self.sock.close()
+            print('Socket Exception: ' + str(e))
 
 
 class TestExecution(S3Activity, GraphiteClient):
 
     def __init__(self, bucket_name, profile, host='localhost'):
-        # super(TestExecution, self).__init__()
         GraphiteClient.__init__(self, host)
         S3Activity.__init__(self, bucket_name, profile)
 
@@ -69,16 +67,22 @@ class TestExecution(S3Activity, GraphiteClient):
             f.write(str(file_content) * size)
         return random_file_name
 
+    def __del__(self):
+        GraphiteClient.__del__(self)
+
 
 def main():
     profile = 'dev'
     cloverleaf = TestExecution('syapse-performance-results', profile)
     folder = 'Test_Upload/'
 
-    for _ in range(20):
-        filename = cloverleaf.create_temp_file(500, 'uploadfile.txt', 's')
-        cloverleaf.upload_s3(filename, folder)
-        os.remove(filename)
+    for i in cloverleaf.get_s3_details(folder):
+        item = i.split()
+        path = 'Metrics.Test.' + item[0].replace('.', '-')
+        print(item[0], item[1], item[2])
+        cloverleaf.send_message(path, item[1], item[2])
+
+    del cloverleaf
 
 
 if __name__ == '__main__':
